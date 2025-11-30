e12d950adbc57e17bbf1b0aaa714df9a7279810d
Wilber Ticona <wilber.ticona@outlook.com>
2025-11-29 21:15:16 -0500

    feat: Add comment to DeckRepository
    A comment was added to the `DeckRepository` class to explain its purpose of interacting with the card deck API.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
--- app/src/main/java/dev/wticona/a23100198pc02moviles/presentation/home/HomeScreen.kt
+++ app/src/main/java/dev/wticona/a23100198pc02moviles/presentation/home/HomeScreen.kt
@@ -8,7 +8,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import dev.wticona.a23100198pc02moviles.presentation.navigation.Routes
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(nav: NavController, vm: HomeViewModel = viewModel()) {
 
